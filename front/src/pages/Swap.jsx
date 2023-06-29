adf76f2d2f2fa2c79e6238c03457d28696fc7fbc
Hynn <choihwoong@me.com>
2023-06-29 16:41:30 +0900

    feat:mypage->asserts

---
+++ front/src/pages/Swap.jsx
@@ -0,0 +1,16 @@
+import { useRecoilState } from "recoil"
+import { loadingState } from "../organisms/store"
+import { Loader } from "../organisms/components"
+
+export const Swap = () =>{
+    const [isLoading] = useRecoilState(loadingState);
+    return (
+        <>
+          {isLoading ? (<Loader />) : (
+            <>
+                <div> test 123</div>
+            </>
+          )}
+        </>
+    )
+}
