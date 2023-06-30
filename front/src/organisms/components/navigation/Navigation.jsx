f01335e9d61eca1deffd20fa3ee93a383ef639fe
Hynn <116252978+Hynn-Hyunwoong@users.noreply.github.com>
2023-06-30 12:39:05 +0900

    Update Navigation.jsx

--- front/src/organisms/components/navigation/Navigation.jsx
+++ front/src/organisms/components/navigation/Navigation.jsx
@@ -1,10 +1,8 @@
 import { NavWrap, NavLi, NavUl, SubLi } from "./styled";
 import { useState } from "react";
-// import { MenuIcon, XIcon } from "@heroicons/react/solid";
 
 export const Navigation = () => {
-  const [isOpen, setMenu] = useState(false);
-  const [activeSubMenu, setActiveSubMenu] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   const category = [
     { label: "내자산", path: "/assets" },
@@ -21,72 +19,34 @@ export const Navigation = () => {
     { label: "대시보드", path: "/dashboard" },
   ];
 
-  const handleMouseEnter = (index) => {
-    setMenu(true);
-    setActiveSubMenu(index);
+  const overEvent = (e) => {
+    e.preventDefault();
+    setIsOpen(true);
   };
 
-  const handleMouseLeave = () => {
-    setMenu(false);
-    setActiveSubMenu(null);
+  const outEvent = (e) => {
+    e.preventDefault();
+    setIsOpen(false);
   };
 
-  const renderSubMenu = (subMenu) => {
-    return subMenu.map((item, index) => (
-      <SubLi key={index} className="sub">
-        {item.label}
+  const categoryMap = category.map((item, index) => {
+    const subMenu = item.subMenu?.map((subItem, subIndex) => (
+      <SubLi key={`${item.label}-${subIndex}`} className="sub">
+        {subItem.label}
       </SubLi>
     ));
-  };
 
-  const renderCategory = () => {
-    return category.map((item, index) => (
-      <NavLi
-        key={index}
-        onMouseEnter={() => handleMouseEnter(index)}
-        onMouseLeave={handleMouseLeave}
-      >
+    return (
+      <NavLi key={item.label} onMouseOver={overEvent} onMouseOut={outEvent}>
         {item.label}
-        {isOpen && activeSubMenu === index && (
-          <div className="subBox">
-            <ul>{item.subMenu && renderSubMenu(item.subMenu)}</ul>
-          </div>
-        )}
+        {isOpen ? <div className="subBox">{item.subMenu && <ul>{subMenu}</ul>}</div> : null}
       </NavLi>
-    ));
-  };
+    );
+  });
 
   return (
     <NavWrap>
-      <NavUl>{renderCategory()}</NavUl>
+      <NavUl>{categoryMap}</NavUl>
     </NavWrap>
   );
 };
-
-/*
-  // const overEvent = (e) => {
-  //   e.preventDefault();
-  //   setMenu(true);
-  // };
-  // const outEvent = (e) => {
-  //   e.preventDefault();
-  //   setMenu(false);
-  // };
-
-  // const [subMenuMap] = category
-  //   .filter((item) => item.hasOwnProperty("subMenu"))
-  //   .map((v) => v.subMenu);
-
-  // const subMenu = subMenuMap.map((v) => (
-  //   <SubLi className="sub">{v.label}</SubLi>
-  // ));
-
-  // const categoryMap = category.map((v, index) => {
-  //   return (
-  //     <NavLi key={index} onMouseOver={overEvent} onMouseOut={outEvent}>
-  //       {v.label}
-  //       {isOpen ? <div className="subBox">{v.subMenu && subMenu}</div> : null}
-  //     </NavLi>
-  //   );
-  // });
-*/
