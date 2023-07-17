654640af8dc8e8d93f427f4ec67f106291d5b89e
mood <mooddddd@naver.com>
2023-07-17 12:41:17 +0900

    fix: footer css 수정 완료

--- front/src/organisms/layouts/baseLayout/footer/Footer.jsx
+++ front/src/organisms/layouts/baseLayout/footer/Footer.jsx
@@ -8,28 +8,38 @@ import {
 } from './styled';
 
 export const Footer = () => {
-  const navigate = useNavigate();
+  const member = [
+    { name: 'Choi HW', github: 'Hynn-Hyunwoong' },
+    { name: 'Lee EJ', github: 'mooddddd' },
+    { name: 'Lee SW', github: 'o-ogie' },
+  ];
+
+  const memberList = member.map((v) => {
+    const clickHandler = (e) => {
+      window.open(`https://github.com/${v.github}`);
+    };
+
+    return (
+      <MemberInfo onClick={clickHandler}>
+        <img src='/images/logo-github.png' />
+        <span>{v.name}</span>
+      </MemberInfo>
+    );
+  });
+
   return (
     <>
       <FooterWrap>
         <ContentWrap>
           <CopyrightSection>
-            <span>ⓒKIWeb-TEAM4. All Rights Reserved</span>
+            <span>
+              <h1>SOLAR swap</h1>
+            </span>
+            <span className='copyright'>
+              ⓒ2023 KIWeb-TEAM4 All Rights Reserved
+            </span>
           </CopyrightSection>
-          <NameSection>
-            <MemberInfo>
-              <img src='/images/logo-github.png' />
-              <span>최현웅</span>
-            </MemberInfo>
-            <MemberInfo>
-              <img src='/images/logo-github.png' />
-              <span>이은지 </span>
-            </MemberInfo>
-            <MemberInfo>
-              <img src='/images/logo-github.png' />
-              <span>이세욱</span>
-            </MemberInfo>
-          </NameSection>
+          <NameSection>{memberList}</NameSection>
         </ContentWrap>
       </FooterWrap>
     </>
